9dc7eec45f2883ef1365022b3b39da0abd6bacfb
Renat Nurgaliyev <r.nurgaliyev@syseleven.de>
2019-03-18 10:29:25 +0100

    Replace ssh key with placeholder in simple instance example

--- simple-instance/vars.tf
+++ simple-instance/vars.tf
@@ -7,3 +7,10 @@ variable "base_image" {
   type        = "string"
   description = "base image id"
 }
+
+variable "ssh_publickey" {
+  type        = "string"
+  description = "ssh-rsa public key in authorized_keys format (ssh-rsa AAAAB3Nz [...] ABAAACAC62Lw== user@host)"
+
+  # default = "ssh-rsa AAAAB3Nz [...] ABAAACAC62Lw== user@host"
+}
