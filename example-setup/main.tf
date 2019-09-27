1b24689829aa0eee33761c1cd4e10da2ebe12863
Matthias Baur <m.baur@syseleven.de>
2019-09-27 14:34:36 +0200

    Add visibility selector for data.openstack_images_image_v2
    The current image select parameters might select a snapshot, which is
    normally not what you expect from this code. This change adds a
    visibility selector for "public" images. Because those can only be
    created by OpenStack admins, it more accurately determines the cloud
    image.

--- example-setup/main.tf
+++ example-setup/main.tf
@@ -72,6 +72,7 @@ data "openstack_networking_network_v2" "public_network" {
 data "openstack_images_image_v2" "image" {
   most_recent = true
 
+  visibility = "public"
   properties = {
     os_distro  = "ubuntu"
     os_version = "16.04"
--- simple-instance/image.tf
+++ simple-instance/image.tf
@@ -1,6 +1,7 @@
 data "openstack_images_image_v2" "image" {
   most_recent = true
 
+  visibility = "public"
   properties = {
     os_distro  = "ubuntu"
     os_version = "16.04"
