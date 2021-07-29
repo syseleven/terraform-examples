623bd6954c39a2fedd56483969b622ff294de636
Lars Grebe <l.grebe@syseleven.de>
2021-07-29 16:18:45 +0200

    update simple instance ipv4 template to work with latest terraform

--- simple-instance/instaces-blue.tf
+++ simple-instance/instaces-blue.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "instance_blue" {
   image_id        = data.openstack_images_image_v2.image.id
   flavor_name     = "m1.tiny"
   key_pair        = openstack_compute_keypair_v2.kp_adminuser.name
-  security_groups = [openstack_compute_secgroup_v2.sg_ssh.name]
+  security_groups = [openstack_networking_secgroup_v2.sg_ssh.name]
 
   network {
     uuid = openstack_networking_network_v2.net_blue.id
--- simple-instance/security-groups.tf
+++ simple-instance/security-groups.tf
@@ -1,12 +1,22 @@
-resource "openstack_compute_secgroup_v2" "sg_ssh" {
-  name        = "simple_sg_ssh"
-  description = "Allow inbound SSH"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
+resource "openstack_networking_secgroup_v2" "sg_ssh" {
+  name        = "simple_sg"
+  description = "Allow inbound SSH/ICMP for IPv4 and IPv6"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "sg_ipv4_tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.sg_ssh.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "sg_ipv4_icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.sg_ssh.id}"
+}
