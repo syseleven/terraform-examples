6bdbb569ebe08fd645fba790757824c0b6039284
najsyseleven <120575124+najsyseleven@users.noreply.github.com>
2023-01-23 11:52:57 +0100

    Change SSH Public Key Variable Name

--- vpnaas/main.tf
+++ vpnaas/main.tf
@@ -5,7 +5,7 @@ variable "ipsec_psk" {
 }
 
 # Public key to access example instances
-variable "public_key" {
+variable "ssh_publickey" {
   type        = string
   description = "ssh-rsa public key in authorized_keys format (ssh-rsa AAAAB3Nz [...] ABAAACAC62Lw== user@host)"
   # default = "ssh-rsa AAAAB3Nz [...] ABAAACAC62Lw== user@host"
