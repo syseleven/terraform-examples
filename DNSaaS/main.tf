aace02a3b4808d031fe65d7e0c04003f484cb36c
Matthias Bernhardt <40292398+matthiasbernhardt@users.noreply.github.com>
2023-05-04 17:25:58 +0200

    os-16472 add example for long DNS TXT records (DKIM/SPF)

--- DNSaaS/main.tf
+++ DNSaaS/main.tf
@@ -1,17 +1,29 @@
 resource "openstack_dns_zone_v2" "example_zone" {
   name        = "example.com."
-  email       = "email2@example.com"
-  description = "a zone"
+  email       = "email@example.com"
+  description = "An example dns zone"
   ttl         = 6000
   type        = "PRIMARY"
 }
 
 resource "openstack_dns_recordset_v2" "rs_example_com" {
   zone_id     = openstack_dns_zone_v2.example_zone.id
-  name        = "rs.example.com."
-  description = "An example record set"
+  name        = "rs.${openstack_dns_zone_v2.example_zone.name}"
+  description = "An example dns record set"
   ttl         = 3000
   type        = "A"
   records     = ["10.0.0.1"]
+}
+
+resource "openstack_dns_recordset_v2" "lr_example_com" {
+  zone_id     = openstack_dns_zone_v2.example_zone.id
+  name        = "lr.${openstack_dns_zone_v2.example_zone.name}"
+  description = "An example for long txt records, one with the maximum unquoted unsplit length of 255 characters and one with 256 characters, splitted and quoted appropriately"
+  ttl         = 3000
+  type        = "TXT"
+  records = [
+    "123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+    "\"123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcd\" \"ef0\""
+  ]
 }
 
